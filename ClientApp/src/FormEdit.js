7bad72879a26cf7f6246b90b80c7a8b7ae26466f
Bradley-Miller <Bradley.miller-3@selu.edu>
2022-10-27 17:37:16 -0500

    FormID and Changing stuff
    We are now fetching the formId on form creation and we're able to now add stuff to the google from using batchUpdate.
    
    There is a problem in regards to the stuff dealing with sessionstorage and keeping stuff around so let's try to fix that stuff in the future.

--- ClientApp/src/FormEdit.js
+++ ClientApp/src/FormEdit.js
@@ -1,12 +1,139 @@
+import { gapi } from 'gapi-script';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 import './FormEditcss.css'
 
+//This function is going to be how we update the form.
+//NEEDS THE FORM ID TO WORK
+
+
+var textUpdateBody = {
+    "includeFormInResponse": false,
+    "requests": [
+      {
+        "createItem": {
+          "item": {
+            "questionItem": {
+              "question": {
+                "textQuestion": {
+                  "paragraph": false
+                }
+              }
+            }
+          },
+          "location": {
+            "index": 0
+          }
+        }
+      }
+    ],
+}
+
+
+var scaleUpdateBody = {
+  "includeFormInResponse": false,
+  "requests": [
+    {
+      "createItem": {
+        "item": {
+          "questionItem": {
+            "question": {
+              "scaleQuestion": {
+                "low": 0,
+                "high": 10,
+                "highLabel": "High",
+                "lowLabel": "Low",
+
+              }
+            }
+          }
+        },
+        "location": {
+          "index": 0
+        }
+      }
+    }
+  ],
+}
+
+var MultiChoiceUpdateBody = {
+  "includeFormInResponse": false,
+  "requests": [
+    {
+      "createItem": {
+        "item": {
+          "questionItem": {
+            "question": {
+              "choiceQuestion": {
+                "options": [{
+                  "value": "a"
+                },
+                  {
+                    "value": "b"
+                  }
+              ],
+              "type": "RADIO"
+              }
+            }
+          }
+        },
+        "location": {
+          "index": 0
+        }
+      }
+    }
+  ],
+}
+
+function textUpdate(){
+    gapi.client.request({
+      path: 'https://forms.googleapis.com/v1/forms/'+sessionStorage.getItem("currentFormId")+':batchUpdate',
+      method: 'POST',
+      body: textUpdateBody,
+      headers: {
+        "Content-type": "application/json",
+    },
+}).then(function(response) { return response.json;
+     },            
+        function(err) { console.error("form not created");});
+    }
+
+function multiUpdate(){
+    gapi.client.request({
+      path: 'https://forms.googleapis.com/v1/forms/'+sessionStorage.getItem("currentFormId")+':batchUpdate',
+      method: 'POST',
+      body: MultiChoiceUpdateBody,
+      headers: {
+        "Content-type": "application/json",
+    },
+}).then(function(response) { return response.json;
+      },            
+        function(err) { console.error("form not created");});
+    }
+
+function scaleUpdate(){
+      gapi.client.request({
+        path: 'https://forms.googleapis.com/v1/forms/'+sessionStorage.getItem("currentFormId")+':batchUpdate',
+        method: 'POST',
+        body: scaleUpdateBody,
+        headers: {
+          "Content-type": "application/json",
+      },
+  }).then(function(response) { return response.json;
+        },            
+          function(err) { console.error("form not created");});
+      }
+
+
+
 //Dakota- This is a basic prototype form consisting of text boxes asking for basic information such as email, first and last name, and favorite food
 //The goal for the futre is to impliment images, videos, and a code editor to execute programs, as well as processes that allow for consistency 
 //when taking in data, for example making sure the first letter is capitalized
 function FormPrototype() {
+  
+  console.log(sessionStorage.getItem("currentFormId"));
+
   return (
     
     <Form>
@@ -35,6 +162,20 @@ function FormPrototype() {
 
       <Button variant="primary" type="submit">
         Submit
+      </Button>
+      </div>
+
+    <div className='buttons'>
+      <Button className = "CreateTextQuestion" onClick={textUpdate}>
+        Create Text Question
+      </Button>
+
+      <Button className = "CreateMultiQuestion" onClick={multiUpdate}>
+        Create Multi-choice Question
+      </Button>
+
+      <Button className = "CreateScaleQuestion" onClick={scaleUpdate}>
+        Create Scale Question
       </Button>
       </div>
     </Form>
