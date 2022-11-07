f6f34fcbeb37048be59cb11ec10879317173e450
Ismael Reyes <ismael.reyes@selu.edu>
2022-11-06 20:28:20 -0600

    Server changes

--- MySQLConnector.cs
+++ MySQLConnector.cs
@@ -13,17 +13,18 @@ namespace LeoForms
         {
             string connStr = "server=database-1.cy1m3s5bdkpt.us-east-2.rds.amazonaws.com;port=3306;database=roaringdb;user=admin;password=DargonBallS051701";
             MySqlConnection conn = new MySqlConnection(connStr);
-            try{
-
-            Console.WriteLine("Connecting to MySQL...");
             conn.Open();
-
-        }
-
-            catch (Exception ex)
+            string query = "select * from Users";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
             {
-                Console.WriteLine(ex.ToString());
+                Console.WriteLine(reader["UserID"]);
+                Console.WriteLine(reader["FirstName"]);
+                Console.WriteLine(reader["LastName"]);
+                Console.WriteLine(reader["Email"]);
             }
+
 
             conn.Close();
             Console.WriteLine("Done.");
